a5d5793ac45be4a1e1f610c2b883693ea2581aef
Fitrox <fitroxrox@gmail.com>
2018-10-22 13:54:00 -0300

    Support for TMPro
    Copy and replace files.

---
+++ Text Mesh Pro Files/Assets/Scripts/LocalisedDropdown.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[AddComponentMenu ("UI/Dropdown Localised")]
+public class LocalisedDropdown : MonoBehaviour
+{
+	private Dropdown m_dropdown;
+	private TMP_Dropdown m_TMPdropdown;
+
+	public string[] optionLocalisedKeys;
+
+	private void Awake ()
+	{
+		m_dropdown = GetComponent<Dropdown> ();
+		m_TMPdropdown = GetComponent<TMP_Dropdown> ();
+
+		if (!m_dropdown && !m_TMPdropdown)
+		{
+			Debug.LogWarning ("No dropdown component found. Be sure it is not being destroyed.");
+		}
+	}
+
+	void Start ()
+	{
+		SetOptionValues ();
+	}
+
+	[System.Diagnostics.Conditional ("UNITY_EDITOR")]
+	void Reset ()
+	{
+		Dropdown dropdownDefault = GetComponent<Dropdown> ();
+		TMP_Dropdown dropdownTMP = GetComponent<TMP_Dropdown> ();
+
+		if (dropdownDefault == null && dropdownTMP == null)
+		{
+			if (UnityEditor.EditorUtility.DisplayDialog ("Choose a Component", "You are missing one of the required componets. Please choose one to add", "Dropdown", "TextMeshPro - Dropdown"))
+			{
+				gameObject.AddComponent<Dropdown> ();
+			}
+			else
+			{
+				gameObject.AddComponent<TMP_Dropdown> ();
+			}
+		}
+	}
+
+	void Update ()
+	{
+
+	}
+
+	public void SetOptionValues ()
+	{
+		if (m_TMPdropdown)
+			UpdateTMP ();
+
+		if (m_dropdown)
+			UpdateDefault ();
+	}
+
+	private void UpdateDefault ()
+	{
+		CheckOptionList (m_dropdown.options.Count);
+
+		m_dropdown.captionText.text = GUITranslator.Instance.GetLocalisedText (optionLocalisedKeys[m_dropdown.value]);
+
+		for (int i = 0; i < m_dropdown.options.Count; i++)
+		{
+			m_dropdown.options[i].text = GUITranslator.Instance.GetLocalisedText (optionLocalisedKeys[i]);
+		}
+	}
+
+	private void UpdateTMP ()
+	{
+		CheckOptionList (m_TMPdropdown.options.Count);
+
+		m_TMPdropdown.captionText.text = GUITranslator.Instance.GetLocalisedText (optionLocalisedKeys[m_TMPdropdown.value]);
+
+		for (int i = 0; i < m_TMPdropdown.options.Count; i++)
+		{
+			m_TMPdropdown.options[i].text = GUITranslator.Instance.GetLocalisedText (optionLocalisedKeys[i]);
+		}
+	}
+
+	private bool CheckOptionList (int dropdownOptionsCount)
+	{
+		if (optionLocalisedKeys.Length == dropdownOptionsCount)
+			return true;
+
+		if (optionLocalisedKeys.Length < dropdownOptionsCount)
+		{
+			Debug.LogWarning ("Not every item in dropdown has a localised key. Some items will not be translated");
+		}
+
+		if (optionLocalisedKeys.Length > dropdownOptionsCount)
+		{
+			Debug.LogWarning ("There are more keys than items in the dropdown. Make sure the option list is not being altered.");
+		}
+
+		return false;
+	}
+
+	public Dropdown Dropdown
+	{
+		get
+		{
+			return m_dropdown;
+		}
+	}
+}
---
+++ Text Mesh Pro Files/Assets/Scripts/LocalisedText.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using LocalisationAndTranslation;
+using TMPro;
+
+[AddComponentMenu ("UI/Text Translatable")]
+public class LocalisedText : MonoBehaviour, ILocalisedObject
+{
+	/// <summary> Entry key Identifiable. </summary>
+	[Tooltip ("Key of text to be translated. Must be unique.")]
+	[SerializeField]
+	private string key;
+
+	private EntryType type = EntryType.Text;
+
+	void Start ()
+	{
+		GUITranslator.Instance.AddLocalisedObject (this);
+	}
+
+	[System.Diagnostics.Conditional ("UNITY_EDITOR")]
+	void Reset ()
+	{
+		Text textComponent = GetComponent<Text> ();
+		TMP_Text tmpComponent = GetComponent<TMP_Text> ();
+
+		if (textComponent == null && tmpComponent == null)
+		{
+			if (UnityEditor.EditorUtility.DisplayDialog ("Choose a Component", "You are missing one of the required componets. Please choose one to add", "Text", "TextMeshPro - Text"))
+			{
+				gameObject.AddComponent<Text> ();
+			}
+			else
+			{
+				gameObject.AddComponent<TextMeshProUGUI> ();
+			}
+		}
+	}
+
+	/// <summary>
+	/// Sets content for Text Component
+	/// </summary>
+	/// <param name="content"></param>
+	public void Set (string content)
+	{
+		Text text = gameObject.GetComponent<Text> ();
+		TMP_Text TMPtext = GetComponent<TMP_Text> ();
+
+		if (text != null)
+		{
+			text.text = content;
+		}
+		else if (TMPtext != null)
+		{
+			TMPtext.text = content;
+		}
+		else
+		{
+			Debug.LogWarningFormat ("LocalisedObject '{0}' has no text component associated.", gameObject.name);
+		}
+	}
+
+	public string Key
+	{
+		get
+		{
+			return key;
+		}
+
+		set
+		{
+			key = value;
+		}
+	}
+
+	public EntryType Type
+	{
+		get
+		{
+			return type;
+		}
+	}
+}
